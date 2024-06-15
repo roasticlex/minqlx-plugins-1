b115b812149505e6e2b7bad0ee51f307ba5bbe1c
roasticlex <62638338+roasticlex@users.noreply.github.com>
2024-06-15 19:27:00 -0300

    Update educator_inferno.py

--- educator_inferno.py
+++ educator_inferno.py
@@ -12,13 +12,13 @@ class educator_inferno(minqlx.Plugin):
         minqlx.console_print("EducatOR inferno plugin unloaded")
   
     def cmd_educator_inferno(self, player, msg, channel):
-        if player.steam_id == "76561197960782507":
+        if player.steam_id == "":
             # Send a command to the client to set EduCatOR's model on fire
             player.send_client_command("r_flash 1 100 255 0 0")
             channel.reply("EduCatOR is now engulfed in flames!")
 
     def cmd_educator_extinguish(self, player, msg, channel):
-        if player.steam_id == "76561197960782507":
+        if player.steam_id == "":
             # Send a command to the client to remove the fire effect
             player.send_client_command("r_flash 0")
             channel.reply("The flames have been extinguished!")
