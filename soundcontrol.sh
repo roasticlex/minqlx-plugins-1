68250e0abf804829b047771f1573a7e22a6ec29b
roasticlex <62638338+roasticlex@users.noreply.github.com>
2024-10-06 19:09:40 -0300

    Create soundcontrol.sh

---
+++ soundcontrol.sh
@@ -0,0 +1,8 @@
+mkdir soundcontrol
+cd soundcontrol
+touch soundbans.txt
+touch category_sound_delays.txt
+touch sound_delays.txt
+touch custom_sound_delays.txt
+printf "short\nmedium\nlong" >> sound_delays.txt
+echo "soundcontrol folder and initial config files created"
