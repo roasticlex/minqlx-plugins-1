705e6a2fc8894b3ecf0165b351f287d6767864cf
roasticlex <62638338+roasticlex@users.noreply.github.com>
2024-10-11 19:29:00 -0300

    Update soundcontrol.sh

--- soundcontrol.sh
+++ soundcontrol.sh
@@ -3,7 +3,6 @@ mkdir soundcontrol
 cd soundcontrol
 touch soundbans.txt
 touch category_sound_delays.txt
-touch custom_sound_delays.txt
 touch config.txt
 printf "short\nmedium\nlong\nsoundautobanthreshold,none\nsoundautobanduration,none\n" >> config.txt
 echo "soundcontrol folder and initial config files created"
